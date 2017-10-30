35aa5e3e9c99e6be3be6af7439db40210d160d51
admin <admin@WIN10-2188>
2017-10-30 16:27:43 +0000

    alteracoes!!

---
+++ FormacaoDatabase/Script.PostDeployment1.sql
@@ -0,0 +1,1 @@
+﻿:r .\Scripts\SeedDB.sql
---
+++ FormacaoMCV/Controllers/PaisController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FormacaoMCV;
+
+namespace FormacaoMCV.Controllers
+{
+    public class PaisController : Controller
+    {
+        private NobelEntities db = new NobelEntities();
+
+        // GET: Pais
+        public ActionResult Index()
+        {
+            var pais = db.Pais.Include(p => p.PaisAtual);
+            return View(pais.ToList());
+        }
+
+        // GET: Pais/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pais pais = db.Pais.Find(id);
+            if (pais == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pais);
+        }
+
+        // GET: Pais/Create
+        public ActionResult Create()
+        {
+            ViewBag.PaisAtualId = new SelectList(db.Pais, "PaisId", "Nome");
+            return View();
+        }
+
+        // POST: Pais/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PaisId,Nome,PaisAtualId")] Pais pais)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pais.Add(pais);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PaisAtualId = new SelectList(db.Pais, "PaisId", "Nome", pais.PaisAtualId);
+            return View(pais);
+        }
+
+        // GET: Pais/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pais pais = db.Pais.Find(id);
+            if (pais == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PaisAtualId = new SelectList(db.Pais, "PaisId", "Nome", pais.PaisAtualId);
+            return View(pais);
+        }
+
+        // POST: Pais/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PaisId,Nome,PaisAtualId")] Pais pais)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pais).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PaisAtualId = new SelectList(db.Pais, "PaisId", "Nome", pais.PaisAtualId);
+            return View(pais);
+        }
+
+        // GET: Pais/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pais pais = db.Pais.Find(id);
+            if (pais == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pais);
+        }
+
+        // POST: Pais/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pais pais = db.Pais.Find(id);
+            db.Pais.Remove(pais);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
